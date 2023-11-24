5636eded2b5903b2f091aae48cb39baf26083a0f
nhankunnn <134067006+nhankunnn@users.noreply.github.com>
2023-11-24 16:40:24 +0700

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,53 @@
+<!-- <?php
+     echo"xin chào<br>";
+     
+     echo"hello";
+
+     echo"<br";
+     $name="NDN";
+?> -->
+
+
+<!-- BUỔI 2 -->
+ <?php   
+     // $name=":Nguyễn Đức Nhân";
+     // // nối chuỗi với biến bằng dấu
+     // echo"Chào mừng bạn".$name." đến với"."PHP";
+     // echo"<br>";
+     // // truyền giá trị một biến vào một chuỗi
+     //  echo"tên tôi là: $name";
+     //  echo"<hr>";
+     //  echo'Tên tôi là:Nguyễn Đức Nhân '
+
+     // BIẾN KIỂU NGUYÊN
+//      $variable=34.5;
+//      // echo $variable;
+
+//      var_dump($variable);
+
+//      // kiểm tra một biến có phải là một biến kiểu nguyên không
+//   $resault =is_int($variable);
+
+// //var_dump ($resault);
+
+// if($resault){
+//      echo'$variable là một biến kiểu nguyên';
+// }else{
+//      echo'$variable không phải là một biến kiểu nguyên';
+// }
+// // BIẾN KIỂU NGUYÊN
+   
+
+
+     $a=2;
+     $b="2";
+     if($a===$b){
+          echo"Như Nhau";
+     }else{
+          echo"Khác Nhau" ;
+     }
+
+ ?>
+
+
+
